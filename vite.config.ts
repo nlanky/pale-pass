3b174042a03313fae6a40ad20bab81849e25c5a4
Neil <nlanky@hotmail.co.uk>
2023-05-21 16:16:47 +0100

    Fix GitHub Actions?

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 // PUBLIC MODULES
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-})
+  base: "/pale-pass/",
+});
