6ec55454e68322c1a353f9c4c995a247e4a6751f
Neil Lancaster <nlanky@hotmail.co.uk>
2023-05-27 22:33:00 +0100

    Pause game when modal is open

--- src/features/game/hooks.ts
+++ src/features/game/hooks.ts
@@ -5,20 +5,27 @@ import { useEffect } from "react";
 // Constants
 import { TURN_TIME } from "features/game/constants";
 // Redux
-import { useAppDispatch } from "features/redux/hooks";
+import { useAppDispatch, useAppSelector } from "features/redux/hooks";
+import { selectOpenBuildingModal } from "features/building/buildingSlice";
 import { incrementTurn } from "features/game/gameSlice";
 
 export const useTurnTimer = () => {
   // Hooks
   const dispatch = useAppDispatch();
+  const openModal = useAppSelector(selectOpenBuildingModal);
 
   // Effects
   useEffect(() => {
+    // If modal is open, pause game
+    if (openModal) {
+      return;
+    }
+
     const timer = setInterval(() => {
       dispatch(incrementTurn());
     }, TURN_TIME);
     return () => {
       clearInterval(timer);
     };
-  }, [dispatch]);
+  }, [dispatch, openModal]);
 };
